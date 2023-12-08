7016b15344705586e5bd2b08de07cc742c8b7b5d
Douglas Saturnino <douglas02saturnino@gmail.com>
2023-12-08 09:39:43 -0300

    Flask Renderizado

--- Projects_Web/Flask/hello_flask/app.py
+++ Projects_Web/Flask/hello_flask/app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_word():
     return "Hello, Word!"
+
+@app.route("/bye")
+def bye():
+    return "Bye!"
+
+@app.route("/username/<name>/<int:number>")
+def greet(name,number):
+    return f"Hello there {name}, you are {number} years old!"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
