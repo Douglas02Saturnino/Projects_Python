7880d123808df6a4d4496972803e4f7fea7e2918
Douglas Saturnino <douglas02saturnino@gmail.com>
2023-12-12 14:53:21 -0300

    jinja introduction

--- Projects_Web/Flask/jinja-introduction/server.py
+++ Projects_Web/Flask/jinja-introduction/server.py
@@ -23,5 +23,13 @@ def guess(name):
     age = age_data["age"]
     return render_template("guess.html", person_name=name, gender=gender, age=age)
 
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 # if __name__ == "__main__":
 #     app.run(debug=True)
